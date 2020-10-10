b03f6d7d069dfc3b75a55d2a2a3d335ef155c948
FabriBertani <fabri_bertani@hotmail.com>
2020-10-10 20:52:44 -0300

    Updated sample project.

--- TestApp/TestApp/App.xaml.cs
+++ TestApp/TestApp/App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace TestApp
 {
@@ -15,14 +14,26 @@ namespace TestApp
 
         protected override void OnStart()
         {
+            base.OnStart();
+
+            if (!Preferences.Get("application_rated", false))
+            {
+                var counter = Preferences.Get("application_counter", 1);
+
+                counter += 1;
+
+                Preferences.Set("application_counter", counter);
+            }
         }
 
         protected override void OnSleep()
         {
+            base.OnSleep();
         }
 
         protected override void OnResume()
         {
+            base.OnResume();
         }
     }
 }
--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Plugin.XamarinAppRating;
 using System;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace TestApp
@@ -10,12 +11,46 @@ namespace TestApp
         public MainPage()
         {
             InitializeComponent();
+
+            if (!Preferences.Get("application_rated", false))
+                Task.Run(() => CheckAppCountAndRate());
+        }
+
+        private async Task CheckAppCountAndRate()
+        {
+            if (Preferences.Get("application_counter", 0) >= 5)
+            {
+                if (!await DisplayAlert("Rate this App!", "Are you enjoying the app so far? Would you like to leave a review in the store?", "Yes", "No"))
+                {
+                    Preferences.Set("application_counter", 0);
+
+                    return;
+                }
+
+                await RateApplication();
+            }
+        }
+
+        private Task RateApplication()
+        {
+            if (CrossAppRating.IsSupported)
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    // This method use Facebook™'s store apps as example.
+                    await CrossAppRating.Current.PerformPlatformRateAppAsync("com.facebook.katana", "id284882215", "9wzdncrf0083");
+                });
+
+                Preferences.Set("application_rated", true);
+            }
+
+            return Task.FromResult(0);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            // This method use Facebook™'s store apps as example.
-            Device.BeginInvokeOnMainThread(async () => await CrossAppRating.Current.PerformPlatformRateAppAsync("com.facebook.katana", "id284882215", "9wzdncrf0083"));
+            if (!Preferences.Get("application_rated", false))
+                Task.Run(() => RateApplication());
         }
     }
 }
