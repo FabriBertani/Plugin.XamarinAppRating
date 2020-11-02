b6c37f3f75456262ed9599e4befeab8f599538d1
FabriBertani <fabri_bertani@hotmail.com>
2020-11-02 16:33:03 -0300

    Added TestAppNative.Android, TestAppNative.iOS and TestAppNative.UWP projects to showcase rating implementation on native platforms.

---
+++ TestAppNative/TestAppNative.Android/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Plugin.XamarinAppRating;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace TestAppNative.Android
+{
+    [Activity(Label = "TestAppNative", Icon = "@mipmap/ic_launcher", Theme = "@style/AppTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : AppCompatActivity, View.IOnClickListener
+    {
+        private Button rateAppOnStoreBtn;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Platform.Init(this, savedInstanceState);
+            
+            SetContentView(Resource.Layout.activity_main);
+
+            rateAppOnStoreBtn = FindViewById<Button>(Resource.Id.rate_on_store_btn);
+
+            rateAppOnStoreBtn.SetOnClickListener(this);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnClick(View v)
+        {
+            if (v.Id == Resource.Id.rate_on_store_btn)
+                MainThread.InvokeOnMainThreadAsync(async () => await RateAppOnStore());
+        }
+
+        private async Task RateAppOnStore()
+        {
+            // This method use Facebook™'s store apps as example.
+            if (CrossAppRating.IsSupported)
+                await CrossAppRating.Current.PerformRatingOnStoreAsync(packageName: "com.facebook.katana");
+        }
+    }
+}
---
+++ TestAppNative/TestAppNative.UWP/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Plugin.XamarinAppRating;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Essentials;
+
+namespace TestAppNative.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RateAppOnStore_Click(object sender, RoutedEventArgs e)
+        {
+            if (CrossAppRating.IsSupported)
+                Task.Run(() => RateAppOnStore());
+        }
+
+        private async Task RateAppOnStore()
+        {
+            // This method use Facebook™'s store apps as example.
+            await CrossAppRating.Current.PerformRatingOnStoreAsync(productId: "9wzdncrf0083");
+        }
+    }
+}
