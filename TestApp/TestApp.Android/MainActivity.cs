faeb7734bf11618bac7cc955caf7e90c6f4a3f84
FabriBertani <fabri_bertani@hotmail.com>
2021-04-28 01:35:50 -0300

    Updated TestApp to Xamarin.Forms 5.0.0.2012 and Xamarin.Essentials to 1.6.1. Migrated TestApp.Android project to AndroidX

--- TestApp/TestApp.Android/MainActivity.cs
+++ TestApp/TestApp.Android/MainActivity.cs
@@ -1,15 +1,16 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace TestApp.Droid
 {
-    [Activity(Label = "TestApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(
+        Label = "TestApp",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
